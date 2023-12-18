766dd69c520037c4f3d16ab93c409a0616de48d4
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-18 17:13:35 +0900

    app.config.js commit

---
+++ src/configs/app.config.js
@@ -0,0 +1,10 @@
+const appConfig = {
+    apiPrefix: '/api',
+    authenticatedEntryPath: '/home',
+    unAuthenticatedEntryPath: '/sign-in',
+    tourPath: '/app/account/kyc-form',
+    locale: 'en',
+    enableMock: false,
+}
+
+export default appConfig
