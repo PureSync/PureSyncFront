c7d45c6c6e7ee469e671405c849ac7402163845b
DESKTOP-COUMCMV\82109 <woosuck6742@naver.com>
2023-12-21 19:08:35 +0900

    fix: repository 파일 최신화 - gitignore 수정 - app.config.js 커밋 - index.css 커밋

---
+++ src/configs/app.config.js
@@ -0,0 +1,10 @@
+const appConfig = {
+    apiPrefix: '/api',
+    authenticatedEntryPath: '/home',
+    unAuthenticatedEntryPath: '/landing',
+    tourPath: '/app/account/kyc-form',
+    locale: 'en',
+    enableMock: false,
+}
+
+export default appConfig
